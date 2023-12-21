a7f8830c5474ef362f1687fce16e8e7db540add3
kodemeex <115670027+kodemeex@users.noreply.github.com>
2023-12-21 20:26:21 +0000

    testing paths

---
+++ mainnode.py
@@ -0,0 +1,22 @@
+### This is the main code for a node in the system
+
+import json
+from gpiozero import LED as GPIO
+from flask import Flask
+app = Flask(__name__)
+nodename = "node1"
+
+led = GPIO(18)
+led.on()
+
+@app.route('/')
+def status_res():
+    status = {"NodeName":"node1", "Light":0}
+    return status
+
+@app.route('/node/status')
+def status_res():
+    status = {"NodeName":"node1", "Light":0}
+    return status
+
+#@app.route('/node/light')
