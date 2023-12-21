32a667c8960fac82e39c396bc81be06fb6176f3b
kodemeex <115670027+kodemeex@users.noreply.github.com>
2023-12-21 21:16:43 +0000

    test turning on lights with new api

--- mainnode.py
+++ mainnode.py
@@ -2,21 +2,51 @@
 
 import json
 from gpiozero import LED as GPIO
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
-nodename = "node1"
 
-led = GPIO(26)
-led.on()
+# Server sets the var's value
+nodename = "node1"
+light_status = [0,0,0,0,0]
+
+
+leds = [GPIO(12),GPIO(27),GPIO(22),GPIO(6),GPIO(25)]
+
+status_led = GPIO(26)
+status_led.on()
 
 @app.route('/')
 def status_res():
-    status = {"NodeName":"node1", "Light":0}
+    status = {"NodeName":nodename, "Lights":light_status}
     return status
 
 @app.route('/node/status')
 def status_result():
-    status = {"NodeName":"node1", "Light":0}
+    status = {"NodeName":nodename, "Light":light_status}
     return status
 
-#@app.route('/node/light')
+@app.route('/node/light')
+def change_light():
+    id = request.args.get('id')
+    action = request.args.get('action')
+    if action == "on":
+        light_on(id)
+    else:
+        light_off(id)
+
+# Button pressed
+# send to server
+
+
+# Change lights status
+def light_on(_id):
+    light_status[_id] = 1
+    leds[_id].on()
+
+def light_off(_id):
+    light_status[_id] = 0
+    leds[_id].off()
+
+def light_toggle(_id):
+    light_status[_id] = (light_status[_id] + 1) % 2 
+    leds[_id].toggle()
