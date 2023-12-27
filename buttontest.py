a85c443c49dc05e1c10067536be5ff654be23b36
kodemeex <115670027+kodemeex@users.noreply.github.com>
2023-12-27 00:33:44 +0000

    test led with button push

--- buttontest.py
+++ buttontest.py
@@ -1,4 +1,4 @@
-import gpiozero
+import gpiozero, requests
 
 button = gpiozero.Button(2)
 led = 0
@@ -8,4 +8,9 @@ while True:
     if button.is_pressed:
         led = 1
     #print(led)
-        
+    if led == 1:
+        r = requests.get("http://192.168.1.204:5000/node/light?id=led0&action=on")
+        print(r.status_code)
+    else:
+        r = requests.get("http://192.168.1.204:5000/node/light?id=led0&action=off")
+        print(r.status_code)
