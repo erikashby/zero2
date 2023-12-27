daf7cf47c8d2bb5548a6b15a26fa05a92f0ea3ca
kodemeex <115670027+kodemeex@users.noreply.github.com>
2023-12-27 00:37:47 +0000

    fix button gets

--- buttontest.py
+++ buttontest.py
@@ -1,16 +1,19 @@
 import gpiozero, requests
 
 button = gpiozero.Button(2)
-led = 0
+button_status = 0
+last_button_status = 0
 
 while True:
-    led = 0
+    button_status = 0
     if button.is_pressed:
-        led = 1
+        button_status = 1
     #print(led)
-    if led == 1:
+    if button_status == 1 & last_button_status == 0:
         r = requests.get("http://192.168.1.204:5000/node/light?id=led0&action=on")
         print(r.status_code)
-    else:
+        last_button_status = 1
+    if button_status == 0 & last_button_status == 1:
         r = requests.get("http://192.168.1.204:5000/node/light?id=led0&action=off")
         print(r.status_code)
+        last_button_status = 0
