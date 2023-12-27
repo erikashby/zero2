4bbe6bcc0cdc3ab17581208f4e65efa69376549c
kodemeex <115670027+kodemeex@users.noreply.github.com>
2023-12-27 22:51:09 +0000

    comment out

--- buttontestput.py
+++ buttontestput.py
@@ -47,7 +47,7 @@ while True:
         print(test_json)
 
         #r = requests.put("http://192.168.1.166:5005/testprint",json=test_json)
-        print(r.status_code)
+        #print(r.status_code)
         last_button_status = 1
     if button_status == 0 and last_button_status == 1:
         last_button_status = 0
