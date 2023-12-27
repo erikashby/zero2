a22985fded625c7fcfe61c11a906ba46e6ca134e
kodemeex <115670027+kodemeex@users.noreply.github.com>
2023-12-27 22:53:36 +0000

    fixed status

--- buttontestput.py
+++ buttontestput.py
@@ -44,10 +44,10 @@ while True:
 
         test_json["status"] = status['status']
 
-        print(test_json)
+        #print(test_json)
 
-        #r = requests.put("http://192.168.1.166:5005/testprint",json=test_json)
-        #print(r.status_code)
+        r = requests.put("http://192.168.1.166:5005/testprint",json=test_json)
+        print(r.status_code)
         last_button_status = 1
     if button_status == 0 and last_button_status == 1:
         last_button_status = 0
