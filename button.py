34ac1904c57218c61a360b8213500bdc26e282d0
kodemeex <115670027+kodemeex@users.noreply.github.com>
2023-12-27 23:28:28 +0000

    fixed button.py

--- button.py
+++ button.py
@@ -2,23 +2,26 @@ import gpiozero, requests, json, socket
 from datetime import datetime
 
 
+### Node varibles
+name = 'zero1'
+version = '0.1'
+
 ### Get IP
 testIP = "8.8.8.8"
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect((testIP, 0))
 myip = s.getsockname()[0]
 s.close()
-### Get IP
 
+### Button varibles
 button = gpiozero.Button(2)
 button_status = 0
 last_button_status = 0
 
 
-
-test_json = {"name":"test", 
+test_json = {"name":name, 
              "type":"node", 
-             "version":"0.1",
+             "version":version,
              "source ip":myip
              }
 
@@ -43,8 +46,6 @@ while True:
         status = requests.get(status_url).json()
 
         test_json["status"] = status['status']
-
-        print(test_json)
 
         r = requests.put("http://192.168.1.166:5005/testprint",json=test_json)
         print(r.status_code)
