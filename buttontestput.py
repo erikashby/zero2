e6891927c3ee4e87f12f86e6c0c9aba4fb7850f5
kodemeex <115670027+kodemeex@users.noreply.github.com>
2023-12-27 19:48:27 +0000

    test get ip with socket

--- buttontestput.py
+++ buttontestput.py
@@ -1,7 +1,14 @@
 import gpiozero, requests, json
-from ipgetter2 import ipgetter1 as ipgetter
 
-myip = ipgetter.myip()
+import socket
+testIP = "8.8.8.8"
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect((testIP, 0))
+myip = s.getsockname()[0]
+host = socket.gethostname()
+print ("IP:", myip, " Host:", host)
+
+
 print(myip)
 
 button = gpiozero.Button(2)
