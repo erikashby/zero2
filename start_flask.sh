2b54452bb0ca73f9fac32ad9cf34a9ce026b3fd1
Erik Ashby <erik@ashbys.net>
2023-12-27 18:06:08 -0700

    Update start_flask.sh

--- start_flask.sh
+++ start_flask.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/erika/app/zero1/
 git pull https://github.com/erikashby/zero1
-. development/bin/activate
+. app/bin/activate
 #export FLASK_APP=mainnode.py
 #flask run --host=0.0.0.0
 
